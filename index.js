bc8524a20aace29bb2aa31508ca7493de33cd18a
Matthew Crooks <mfcrooks@gmail.com>
2018-08-17 20:46:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,4 +18,16 @@ function BoardMember(name, homeState, training) {
   this.releasePressStatement = function () {
     return 'You will see great things from Scuber.';
   }
+
+  this.sayHi = function () {
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+  }
+}
+
+function User (name, email){
+  this.name = name;
+  this.email = email;
+  this.sayHello = function(){
+    console.log(`Hello everybody, my name is ${this.name}!`);
+  }
 }
